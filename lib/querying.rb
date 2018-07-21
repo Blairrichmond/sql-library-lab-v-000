85c6d9b600fe2ecb2e4a16fbf4de36aad0e464e6
Brent  Ransom <sadumedusa@gmail.com>
2018-07-21 03:09:06 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, COUNT(characters.species) FROM series LEFT JOIN characters ON characters.series_id = series.id GROUP BY characters.species;"
+  "SELECT series.title, COUNT(characters.species) FROM series LEFT JOIN characters ON characters.series_id = series.id GROUP BY characters.species DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
